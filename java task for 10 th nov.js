7597e98aa06e25faf86257c4d2bfaf06cb5c5067
Glory8910 <74110767+Glory8910@users.noreply.github.com>
2020-11-11 07:15:04 -0800

    Update java task for 10 th nov.js

--- java task for 10 th nov.js
+++ java task for 10 th nov.js
@@ -1,26 +1,32 @@
+
 let arr=[1,2,3,4,4,6,5,6];
 
 
 //remove duplicates
 
 let arr=[1,2,3,4,4,6,5,6];
-let nodupe=arr.filter((b,index)=>arr.indexOf(b)===index);
+let nodupe=function(){
+   return arr.filter((b,index)=>arr.indexOf(b)===index);
+}();
+
 nodupe;
+//let nodupe=arr.filter((b,index)=>arr.indexOf(b)===index);
+//nodupe;
 
 //odd numbers of an array
-let odd=arr.filter(ele=>ele%2===1);
+let odd=function(){
+    return arr.filter(ele=>ele%2===1);
+}();
 odd;
 
 //sum of all numbers in an arrar
-let sum=arr.reduce((add,ele)=>add=add+ele,0);
+let sum=function(){
+    return arr.reduce((add,ele)=>add=add+ele,0);
+}();
 sum;
-
 
    //prime numbers
   
-   let prime=arr.filter(ele=>isprime(ele));
-   
-   
    
    function isprime(a)
    {
@@ -32,24 +38,27 @@ sum;
        return false;
    }
     
-   console.log(a);
+ // console.log(a);
    for(i=3;i<Math.sqrt(a);i++){
        if(a%i===0){
        return false;
    }
    }
-   
-   
    return true;
    }
-    prime;
+   let prime=function(){
+    return arr.filter(ele=>isprime(ele));
+}();
+ prime;
    
 
 
 let strarr=['ask','dad','recive','seek','dnnd','find'];
  //all strings to title caps
 
- let caps=strarr.map(ele=>ele.toUpperCase())
+ let caps=function(){
+     return strarr.map(ele=>ele.toUpperCase())
+ }();
  caps;
 
  // palidrome
@@ -67,6 +76,7 @@ sepwords;
 
 
 //median of two sorted array
+
 let a=[9,6,7,1,5];
 let b=[4,6,7,3,4];
 let a1=a.concat(b);
@@ -79,3 +89,19 @@ let median=function(){
     return((a1[mid]+a1[mid-1])/2);
 }();
 median;
+
+
+// rotate k times
+
+let v=[1,4,5,7,4,7,8,33,55,6];
+
+let rotate=5;
+let rotated=function(){
+for(i=0;i<rotate;i++){
+let shiftedv=v.shift();
+v.push(shiftedv);
+}
+console.log(v);
+}();
+
+rotated;
